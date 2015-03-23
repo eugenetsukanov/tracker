cac1403a29f2a90d0130b77c99180a111a4962ba
Feya <iravi93@yandex.ua>
2015-03-23 19:34:11 +0200

    - clean up app.js

--- app.js
+++ app.js
@@ -6,21 +6,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/tracker');
-
-var Schema = mongoose.Schema;
-
-var Task = new Schema({
-    title:  String,
-    user: String,
-    priority: Number,
-    status: String,
-    spendtime: Number,
-    velocity: Number,
-    date: { type: Date, default: Date.now }
-});
-
 
 var server = app.listen(3000, function () {
 
