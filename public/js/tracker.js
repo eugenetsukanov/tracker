6f2b398dda3e0725c5b8d629f5142535483dcf72
Feya <iravi93@yandex.ua>
2015-03-31 15:24:21 +0300

    - refactoring complexity

--- public/js/tracker.js
+++ public/js/tracker.js
@@ -63,7 +63,66 @@ angular
 
         $scope.priorities = [0,1,2,3,4,5,6,7,8,9,10];
 
-        $scope.complexities = [0,1,2,3,4,5,6,7,8,9,10,11];
+        $scope.complexities = [
+            {
+                name: '0',
+                value: 0
+            },
+            {
+                name: '0+',
+                value: 1
+            },
+            {
+                name: '1',
+                value: 2
+            },
+            {
+                name: '1+',
+                value: 3
+            },
+            {
+                name: '2',
+                value: 4
+            },
+            {
+                name: '2+',
+                value: 5
+            },
+            {
+                name: '3',
+                value: 6
+            },
+            {
+                name: '3+',
+                value: 7
+            },
+            {
+                name: '4',
+                value: 8
+            },
+            {
+                name: '4+',
+                value: 9
+            },
+            {
+                name: '5',
+                value: 10
+            },
+            {
+                name: '5+',
+                value: 11
+            },
+        ];
+
+        $scope.complexityHuman = function (complexity) {
+         var result = '';
+          $scope.complexities.forEach(function (item) {
+              if (item.value == complexity) {
+                  result = item.name;
+              }
+          })
+            return result;
+        };
 
         var init = function () {
 
