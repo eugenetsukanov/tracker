d07e7c3b2913050d1a6e111580d91276362145f9
Alex <alexgkorsun@gmail.com>
2015-04-01 17:25:35 +0300

    - add views for tasks (default, list) via buttons

--- public/js/tracker.js
+++ public/js/tracker.js
@@ -130,6 +130,9 @@ angular
         $scope.statuses = [
             "open", "in progress", "done", "accepted"
         ];
+        $scope.loadView = function (view) {
+            $scope.view = view;
+        };
 
 
         $scope.priorities = [0,1,2,3,4,5,6,7,8,9,10];
