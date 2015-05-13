c9f33886b45c02ee646ed3088a405f5267dc1711
eugene <yannesh@gmail.com>
2015-05-13 20:42:37 +0300

    - add directive for task status label

--- public/js/tracker.js
+++ public/js/tracker.js
@@ -362,6 +362,30 @@ angular
             }
         }
     })
+    .directive('taskStatusLabel', function () {
+        return {
+            restrict: 'C',
+            link: function (scope, element, attrs) {
+                var getClass = function () {
+                    var cl = 'label-info';
+
+                    if (scope.task.status == 'accepted') {
+                        cl = 'label-success';
+                    }
+
+                    if (scope.task.status == 'in progress') {
+                        cl = 'label-warning'
+                    }
+
+                    return cl
+
+                };
+
+                attrs.$addClass(getClass());
+
+            }
+        }
+    })
 
 
 ;
