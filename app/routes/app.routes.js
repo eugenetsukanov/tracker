23744bca01e9023ce4de88c2a9c26ac6a48b89f9
slava hatnuke <slava.hatnuke@gmail.com>
2015-05-27 16:37:57 +0300

    - api/upload > api/files

--- app/routes/app.routes.js
+++ app/routes/app.routes.js
@@ -3,6 +3,6 @@ module.exports = function (app, passport) {
     require('../api/auth')(app, passport);
     require('../api/task')(app);
     require('../api/report')(app);
-    require('../api/upload')(app);
+    require('../api/files')(app);
 
 };
