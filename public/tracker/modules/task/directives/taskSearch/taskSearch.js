12bd8838dd728285897eb8f1d6abb872e78640b3
Andrey Leontyev <skaliozz@gmail.com>
2015-06-10 20:45:46 +0300

    - improvements for search (passing through link to task)

--- public/tracker/modules/task/directives/taskSearch/taskSearch.js
+++ public/tracker/modules/task/directives/taskSearch/taskSearch.js
@@ -7,18 +7,26 @@ angular
             templateUrl: 'tracker/modules/task/directives/taskSearch/taskSearch.html',
             controller: function ($scope, Search, foundTasks) {
 
+                var clearFoundTasks = function () {
+                    angular.copy([], foundTasks.items)
+                };
+                $scope.$watch('search', function () {
+                   clearFoundTasks();
+                });
+
                 $scope.searchQuery = function (query) {
 
                     var q = query ? query.trim() : '';
 
                     if (q.length == 0) {
-                        angular.copy([], foundTasks.items);
+                        clearFoundTasks();
                     }
 
                     if (q.length > 0) {
                         Search.query({taskId: $scope.task._id, query: q}, function (tasks) {
                             $scope.foundTasks = tasks;
                             angular.copy(tasks, foundTasks.items);
+
                         });
                     }
                 }
