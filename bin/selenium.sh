979bfde2bae6f383526b5818643647ca78084592
slava hatnuke <slava.hatnuke@gmail.com>
2015-07-06 15:12:54 +0300

    changed selenium and driver versions

--- bin/selenium.sh
+++ bin/selenium.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ls | grep selenium-server-standalone-2.42.2.jar || wget http://selenium-release.storage.googleapis.com/2.42/selenium-server-standalone-2.42.2.jar
-java -jar selenium-server-standalone-2.42.2.jar
+ls | grep selenium-server-standalone-2.46.0.jar || wget http://selenium-release.storage.googleapis.com/2.46/selenium-server-standalone-2.46.0.jar
+java -jar selenium-server-standalone-2.46.0.jar
