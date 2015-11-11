383a902b884a69695ceff940a67ad275f42ecf36
koralex <raver.trancer@gmail.com>
2015-11-11 12:53:11 +0200

    -cleanup

--- public/tracker/tracker.js
+++ public/tracker/tracker.js
@@ -1,6 +1,6 @@
 angular
     .module('Tracker', ['ui.router', 'toaster', 'ngAnimate', 'ngResource', 'ui.bootstrap',
-        'ngFileUpload', 'monospaced.elastic', 'ui.select', 'ngSanitize', 'ngStorage', 'angular-loading-bar', 'ui.tree'])
+        'ngFileUpload', 'monospaced.elastic', 'ui.select', 'ngSanitize', 'ngStorage', 'angular-loading-bar'])
 
     .config(function ($stateProvider, $urlRouterProvider, $httpProvider, cfpLoadingBarProvider) {
 
