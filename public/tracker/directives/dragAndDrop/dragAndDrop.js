9b1de91c703b79c53810aa9936161ab7d47205bc
Irina <irinaudovyk@gmail.com>
2016-09-20 19:12:41 +0300

    - simplefied functionality;

--- public/tracker/directives/dragAndDrop/dragAndDrop.js
+++ public/tracker/directives/dragAndDrop/dragAndDrop.js
@@ -28,52 +28,23 @@ angular
                     return item;
                 };
 
-                function saveChildTask(child, parent) {
-                    child.parentTaskId = parent._id;
-                    delete child.children;
-
-                    var task = new Task(child);
-
-                        task.$update({taskId: task._id}, function () {
-                            updateParentTask(parent)
-                        });
-                }
-
-                function updateParentTask(parent) {
-                    parent.children.splice(0, 1);
-                }
-
-                function updateNestedTask() {
-
-                    _.forEach($scope.tasksList, function (o) {
-                        var parentTask = _.find(o.tasks, function (task) {
-                            return task.children.length > 0
-                        });
-
-                        if (parentTask) {
-                            saveChildTask(parentTask.children[0], parentTask);
-                        }
-
-                    });
-                }
-
-                var nestedTask = null;
-
                 $scope.dropCallbackOfTask = function (event, index, item, external, type) {
+
                     if(item._id === type){
                         return false;
                     }
-                    nestedTask = item;
+
+                    item.parentTaskId = type;
+                    var task = new Task(item);
+                    task.$update({taskId: task._id}, function () {
+                    });
+
                     return item;
                 };
 
 
                 $scope.$watch('tasksList', function (tasksList) {
                     $scope.modelAsJson = angular.toJson(tasksList, true);
-                    if (nestedTask) {
-                        updateNestedTask();
-                        nestedTask = null;
-                    }
 
                 }, true);
 
