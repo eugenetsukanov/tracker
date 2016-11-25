4c983be8f9407cfc678d274b73182c71520ba1f6
slava hatnuke <slava.hatnuke@gmail.com>
2016-11-25 21:27:43 +0200

    - updated chromedriver need for docker needs

--- bin/selenium-linux64.sh
+++ bin/selenium-linux64.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ls | grep chromedriver || wget http://chromedriver.storage.googleapis.com/2.16/chromedriver_linux64.zip && unzip chromedriver_linux64.zip && rm chromedriver_linux64.zip
+ls | grep chromedriver || wget http://chromedriver.storage.googleapis.com/2.25/chromedriver_linux64.zip && unzip chromedriver_linux64.zip && rm chromedriver_linux64.zip
 
 bin/selenium.sh
